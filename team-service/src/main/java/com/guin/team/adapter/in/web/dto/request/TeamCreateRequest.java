3361c140c79ae588a952ef9dd2df203d075a0d8a
Shin seongsu <tlstjdtn321@naver.com>
2023-12-18 01:16:38 +0900

    feat: 팀 생성에 사용될 request 추가

---
+++ team-service/src/main/java/com/guin/team/adapter/in/web/dto/request/TeamCreateRequest.java
@@ -0,0 +1,36 @@
+package com.guin.team.adapter.in.web.dto.request;
+
+import com.guin.team.domain.constant.SubjectType;
+import com.guin.team.domain.constant.TemplateType;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+public record TeamCreateRequest(
+        @NotBlank(message = "제목은 빈값을 넣을 수 없습니다.")
+        String subject,
+        @NotBlank(message = "내용은 빈 값을 넣을 수 없습니다.")
+        String content,
+        @NotBlank(message = "오픈채팅은 빈 값을 넣을 수 없습니다.")
+        String openChatUrl,
+        @NotBlank(message = "주제타입은 빈 값을 넣을 수 없습니다.")
+        SubjectType subjectType,
+        List<TeamCreateTemplateRequest> teamTemplates,
+        List<String> types,
+        List<TeamCreateRoleRequest> roles
+) {
+
+    public record TeamCreateTemplateRequest(
+            TemplateType type,
+            String question,
+            String option
+    ) {
+    }
+
+    public record TeamCreateRoleRequest(
+            String name,
+            int requiredCount
+    ) {
+    }
+
+}
