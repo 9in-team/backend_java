a7c3aeeab92f3db79f19e23ef2429bfbe0a4b108
Shin seongsu <tlstjdtn321@naver.com>
2023-12-18 01:16:49 +0900

    feat: 팀 반환에 추가될 response 추가

---
+++ team-service/src/main/java/com/guin/team/adapter/in/web/dto/response/TeamCreateResponse.java
@@ -0,0 +1,31 @@
+package com.guin.team.adapter.in.web.dto.response;
+
+import com.guin.team.domain.constant.SubjectType;
+import com.guin.team.domain.constant.TemplateType;
+
+import java.util.List;
+
+public record TeamCreateResponse(
+        Long teamId,
+        String openChatUrl,
+        String content,
+        String subject,
+        SubjectType subjectType,
+        List<TeamTemplateDetail> teamTemplates,
+        List<String> types,
+        List<TeamRoleDetail> teamRoleDetail
+) {
+
+    public record TeamTemplateDetail(
+        TemplateType type,
+        String question,
+        String options
+    ) { }
+
+    public record TeamRoleDetail(
+            String name,
+            int requiredCount,
+            int hiredCount
+    ) { }
+
+}
