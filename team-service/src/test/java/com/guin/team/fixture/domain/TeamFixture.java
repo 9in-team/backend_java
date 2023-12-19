473aacf62d026e5e5963316f0712c9e3bf256d33
Shin seongsu <tlstjdtn321@naver.com>
2023-12-19 23:46:46 +0900

    test: 테스트를 쉽게 하기위해 Fixture 추가

---
+++ team-service/src/test/java/com/guin/team/fixture/domain/TeamFixture.java
@@ -0,0 +1,21 @@
+package com.guin.team.fixture.domain;
+
+import com.guin.team.domain.constant.SubjectType;
+import com.guin.team.domain.vo.Team;
+import com.guin.team.fixture.Fixture;
+
+public class TeamFixture extends Fixture {
+
+    public static Team create(final String subject,
+                              final String content,
+                              final SubjectType subjectType,
+                              final String openChatUrl) {
+        return fixtureMonkey.giveMeBuilder(Team.class)
+                .set("subject", subject)
+                .set("content", content)
+                .set("subjectType", subjectType)
+                .set("openChatUrl", openChatUrl)
+                .sample();
+    }
+
+}
