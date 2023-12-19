9d14365d77ad6852fdd2a3e05f0f9de645f77002
Shin seongsu <tlstjdtn321@naver.com>
2023-12-19 23:42:52 +0900

    feat: SubjectType enum 값으로 수정하는 기능 추가

--- team-service/src/main/java/com/guin/team/domain/constant/SubjectType.java
+++ team-service/src/main/java/com/guin/team/domain/constant/SubjectType.java
@@ -1,6 +1,15 @@
 package com.guin.team.domain.constant;
 
+import java.util.Arrays;
+
 public enum SubjectType {
     PROJECT,
-    STUDY
+    STUDY;
+
+    public static SubjectType from(final String type) {
+        return Arrays.stream(values())
+                .filter(value -> value.name().equals(type))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
