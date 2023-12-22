d6f53d3d674c0d4cfe50d9743d17ba8f536401f6
신성수 <tlstjdtn321@naver.com>
2023-12-22 15:52:26 +0900

    feat: TeamEntity에서 hastTagEntity 의존성 주입

--- team-service/src/main/java/com/guin/team/adapter/out/persistence/entity/TeamEntity.java
+++ team-service/src/main/java/com/guin/team/adapter/out/persistence/entity/TeamEntity.java
@@ -1,46 +1,62 @@
 package com.guin.team.adapter.out.persistence.entity;
 
+import com.guin.team.adapter.out.persistence.entity.common.BaseTimeEntity;
 import com.guin.team.domain.constant.SubjectType;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "team")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class TeamEntity {
+public class TeamEntity extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("아이디")
     private Long id;
 
+    @Comment("팀장 아이디")
     @Column(name = "leader_id", nullable = false)
     private Long leaderId;
 
+    @Comment("제목")
     @Column(name = "subject", nullable = false)
     private String subject;
 
+    @Comment("본문")
     @Column(name = "content", nullable = false)
     private String content;
 
-    @Column(name = "Subject_type", nullable = false, length = 10)
+    @Comment("모집글 형태")
+    @Column(name = "subject_type", nullable = false, length = 10)
     @Enumerated(EnumType.STRING)
     private SubjectType subjectType;
 
+    @Comment("오픈 채팅방 URL")
     @Column(name = "open_chat_url", length = 500)
     private String openChatUrl;
+
+    @OneToMany(cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
+    @JoinColumn(name = "team_id")
+    private List<HashTagEntity> hashTags;
 
     public TeamEntity(final Long leaderId,
                       final String subject,
                       final String content,
                       final SubjectType subjectType,
-                      final String openChatUrl) {
+                      final String openChatUrl,
+                      List<HashTagEntity> hashTags) {
         this.leaderId = leaderId;
         this.subject = subject;
         this.content = content;
         this.subjectType = subjectType;
         this.openChatUrl = openChatUrl;
+        this.hashTags = hashTags;
     }
 }
