9dc38616ea01ec1851fe42fcd06c4546261ad928
신성수 <tlstjdtn321@naver.com>
2023-12-22 15:53:08 +0900

    feat: domain 프로퍼티 추가

--- team-service/src/main/java/com/guin/team/domain/vo/Team.java
+++ team-service/src/main/java/com/guin/team/domain/vo/Team.java
@@ -2,11 +2,14 @@ package com.guin.team.domain.vo;
 
 import com.guin.team.domain.constant.SubjectType;
 
+import java.util.List;
+
 public record Team(
     Long id,
     Long leaderId,
     String subject,
     String content,
     SubjectType subjectType,
-    String openChatUrl
+    String openChatUrl,
+    List<String> hashTag
 ) { }
