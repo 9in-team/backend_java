3e50ab6d8646b06492998aa252e16293f16266a5
신성수 <tlstjdtn321@naver.com>
2023-12-22 15:55:39 +0900

    test: Fixture 추가

--- team-service/src/test/java/com/guin/team/fixture/dto/TeamCreateRequestFixture.java
+++ team-service/src/test/java/com/guin/team/fixture/dto/TeamCreateRequestFixture.java
@@ -5,6 +5,7 @@ import com.guin.team.adapter.in.web.dto.request.TeamCreateRequest;
 import com.guin.team.domain.constant.SubjectType;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 public class TeamCreateRequestFixture extends Fixture {
@@ -19,6 +20,20 @@ public class TeamCreateRequestFixture extends Fixture {
                 SubjectType.PROJECT,
                 Collections.emptyList(),
                 Collections.emptyList(),
+                Collections.emptyList()
+        );
+    }
+
+    public static TeamCreateRequest create(final String subject, final List<String> hashTags) {
+        String randomUUID = UUID.randomUUID().toString();
+
+        return new TeamCreateRequest(
+                subject,
+                "content " + randomUUID,
+                randomUUID,
+                SubjectType.PROJECT,
+                Collections.emptyList(),
+                hashTags,
                 Collections.emptyList()
         );
     }
