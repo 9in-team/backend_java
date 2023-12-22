bc9aa318f91deb6bf9798d0718b21a89cfe622e3
신성수 <tlstjdtn321@naver.com>
2023-12-22 15:53:49 +0900

    test: 해시태그가 있는 모집글 생성 인수테스트 추가

--- team-service/src/test/java/com/guin/team/acceptance/TeamAcceptance.java
+++ team-service/src/test/java/com/guin/team/acceptance/TeamAcceptance.java
@@ -3,6 +3,7 @@ package com.guin.team.acceptance;
 import com.guin.team.acceptance.step.TeamControllerStep;
 import com.guin.team.adapter.in.web.dto.request.TeamCreateRequest;
 import com.guin.team.adapter.in.web.dto.response.TeamCreateResponse;
+import com.guin.team.adapter.out.persistence.entity.HashTagEntity;
 import com.guin.team.adapter.out.persistence.entity.TeamEntity;
 import com.guin.team.adapter.out.persistence.repository.TeamRepository;
 import com.guin.team.domain.constant.SubjectType;
@@ -16,6 +17,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TeamAcceptance extends AcceptanceTest {
@@ -23,6 +27,7 @@ public class TeamAcceptance extends AcceptanceTest {
     private final TeamRepository teamRepository;
 
     private TeamCreateRequest request;
+    private TeamCreateRequest existHashTagRequest;
 
     public TeamAcceptance(final TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
@@ -32,6 +37,7 @@ public class TeamAcceptance extends AcceptanceTest {
     void setup() {
         super.setUp();
         this.request = TeamCreateRequestFixture.create("제목");
+        this.existHashTagRequest = TeamCreateRequestFixture.create("제목", List.of("자바", "스프링", "JPA", "MYSQL", "AWS"));
     }
 
     @DisplayName("모집글을 생성한다.")
@@ -66,6 +72,32 @@ public class TeamAcceptance extends AcceptanceTest {
         ExtractableResponse<Response> response = TeamControllerStep.create(errorRequest);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("해시태그가 있는 모집글을 생성한다.")
+    @Test
+    void existHashTagProjectTeam() {
+        JsonPath response = TeamControllerStep.create(existHashTagRequest).jsonPath();
+
+        TeamEntity teamEntity = teamRepository.findAll().get(0);
+
+        TeamCreateResponse actual = TeamCreateResponseFixture.create(
+                teamEntity.getId(),
+                teamEntity.getOpenChatUrl(),
+                teamEntity.getContent(),
+                teamEntity.getSubject(),
+                teamEntity.getSubjectType(),
+                teamEntity.getHashTags().stream().map(HashTagEntity::getHashTag).collect(Collectors.toList())
+        );
+
+        assertThat(actual).isEqualTo(TeamCreateResponseFixture.create(
+                response.getLong("teamId"),
+                response.getString("openChatUrl"),
+                response.getString("content"),
+                response.getString("subject"),
+                SubjectType.from(response.getString("subjectType")),
+                existHashTagRequest.hashTags()
+        ));
     }
 
 }
