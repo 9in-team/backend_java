5c19a70a989333cdc17d26a8f8369ca4c1b68088
Shin seongsu <tlstjdtn321@naver.com>
2023-12-23 10:45:37 +0900

    fix: lazy loading을 위해 트랜잭션 추가

--- team-service/src/test/java/com/guin/team/integration/ServiceTest.java
+++ team-service/src/test/java/com/guin/team/integration/ServiceTest.java
@@ -4,9 +4,11 @@ import com.guin.team.util.DataCleanUp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@Transactional
 public abstract class ServiceTest {
 
     @Autowired
