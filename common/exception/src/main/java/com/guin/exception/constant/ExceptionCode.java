a0cf280059c817534d4e8b24b99bca83c3e2b854
Shin seongsu <tlstjdtn321@naver.com>
2023-12-23 22:52:08 +0900

    feat: 공통 에러 코드 추가

---
+++ common/exception/src/main/java/com/guin/exception/constant/ExceptionCode.java
@@ -0,0 +1,21 @@
+package com.guin.exception.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum ExceptionCode {
+
+    INVALID_FIELD_REQUEST("잘못된 요청값으로 요청하였습니다."),
+    INVALID_REQUEST_BODY("잘못된 형식의 Request Body 입니다."),
+    INVALID_HTTP_METHOD_REQUEST("지원하지 않는 HTTP 메소드 요청입니다."),
+
+    UNAUTHORIZED_KEY("인증되지 않은 액세스 키 혹은 시크릿 키입니다."),
+
+    INTERNAL_SEVER_ERROR("예상하지 못한 서버 에러가 발생했습니다.");
+
+    private final String message;
+
+    ExceptionCode(final String message) {
+        this.message = message;
+    }
+}
