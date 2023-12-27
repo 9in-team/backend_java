797db5ef68c5ce1933de213f23fd73296d5858ff
신성수 <tlstjdtn321@naver.com>
2023-12-27 16:37:41 +0900

    test: command 로 수정해서 작성

--- team-service/src/test/java/com/guin/team/integration/fake/FakeTeamUseCaseTest.java
+++ team-service/src/test/java/com/guin/team/integration/fake/FakeTeamUseCaseTest.java
@@ -1,20 +1,18 @@
 package com.guin.team.integration.fake;
 
-import com.guin.team.adapter.in.web.dto.request.TeamCreateRequest;
 import com.guin.team.adapter.out.persistence.TeamPersistenceAdapter;
 import com.guin.team.adapter.out.persistence.entity.TeamEntity;
 import com.guin.team.adapter.out.persistence.mapper.HashTagMapper;
 import com.guin.team.adapter.out.persistence.mapper.TeamMapper;
 import com.guin.team.adapter.out.persistence.repository.TeamRepository;
+import com.guin.team.application.port.in.TeamUseCase;
+import com.guin.team.application.port.in.command.TeamCommand;
 import com.guin.team.application.service.TeamService;
 import com.guin.team.domain.vo.Team;
-import com.guin.team.fixture.dto.TeamCreateRequestFixture;
+import com.guin.team.fixture.command.TeamCommandFixture;
 import com.guin.team.integration.fake.repository.FakeTeamRepository;
-import com.guin.team.application.port.in.TeamUseCase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,8 +20,7 @@ public class FakeTeamUseCaseTest {
 
     private final TeamUseCase teamUseCase;
     private final TeamRepository teamRepository;
-
-    private TeamCreateRequest request;
+    private TeamCommand command;
 
     public FakeTeamUseCaseTest() {
         this.teamRepository = new FakeTeamRepository();
@@ -35,13 +32,13 @@ public class FakeTeamUseCaseTest {
                 )
         );
 
-        this.request = TeamCreateRequestFixture.create("제목");
+        this.command = TeamCommandFixture.create("제목");
     }
 
     @DisplayName("모집글을 생성한다.")
     @Test
     void createProject() {
-        Team actual = teamUseCase.save(request);
+        Team actual = teamUseCase.save(command);
         TeamEntity teamEntity = teamRepository.findAll().get(0);
 
         assertThat(actual).isEqualTo(new Team(
@@ -51,7 +48,7 @@ public class FakeTeamUseCaseTest {
                 teamEntity.getContent(),
                 teamEntity.getSubjectType(),
                 teamEntity.getOpenChatUrl(),
-                Collections.emptyList()
+                actual.hashTag()
         ));
     }
 
