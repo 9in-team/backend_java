31e704a5ecfeff92d0a674407993138fcd88f801
신성수 <tlstjdtn321@naver.com>
2023-12-27 16:38:13 +0900

    test: TeamCommand Fixture 추가하여, 테스트 코드 수정

---
+++ team-service/src/test/java/com/guin/team/fixture/command/TeamCommandFixture.java
@@ -0,0 +1,30 @@
+package com.guin.team.fixture.command;
+
+import com.guin.team.application.port.in.command.TeamCommand;
+import com.guin.team.fixture.Fixture;
+
+import java.util.List;
+import java.util.UUID;
+
+public class TeamCommandFixture extends Fixture {
+
+    public static TeamCommand create(final String subject) {
+        return fixtureMonkey.giveMeBuilder(TeamCommand.class)
+                .set("subject", subject)
+                .set("hashTags", List.of("스프링"))
+                .set("content", "본문")
+                .sample();
+    }
+
+    public static TeamCommand create(final String subject, final List<String> hashTags) {
+        String randomUUID = UUID.randomUUID().toString();
+
+        return fixtureMonkey.giveMeBuilder(TeamCommand.class)
+                .set("subject", subject)
+                .set("openChatUrl", randomUUID)
+                .set("hashTags", hashTags)
+                .set("content", "본문")
+                .sample();
+    }
+
+}
--- team-service/src/test/java/com/guin/team/integration/inmemory/TeamUseCaseTest.java
+++ team-service/src/test/java/com/guin/team/integration/inmemory/TeamUseCaseTest.java
@@ -1,20 +1,19 @@
 package com.guin.team.integration.inmemory;
 
-import com.guin.team.adapter.in.web.dto.request.TeamCreateRequest;
 import com.guin.team.adapter.out.persistence.entity.HashTagEntity;
 import com.guin.team.adapter.out.persistence.entity.TeamEntity;
 import com.guin.team.adapter.out.persistence.repository.TeamRepository;
 import com.guin.team.application.port.in.TeamUseCase;
+import com.guin.team.application.port.in.command.TeamCommand;
 import com.guin.team.application.service.TeamService;
 import com.guin.team.domain.vo.Team;
-import com.guin.team.fixture.dto.TeamCreateRequestFixture;
+import com.guin.team.fixture.command.TeamCommandFixture;
 import com.guin.team.integration.ServiceTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.dao.DataIntegrityViolationException;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -40,9 +39,9 @@ public class TeamUseCaseTest extends ServiceTest {
     @DisplayName("모집글을 저장한다.")
     @Test
     void save() {
-        TeamCreateRequest request = TeamCreateRequestFixture.create("제목");
+        final TeamCommand command = TeamCommandFixture.create("제목");
 
-        Team team = teamUseCase.save(request);
+        Team team = teamUseCase.save(command);
 
         TeamEntity teamEntity = teamRepository.findById(team.id())
                 .orElseThrow(RuntimeException::new);
@@ -54,26 +53,26 @@ public class TeamUseCaseTest extends ServiceTest {
                 teamEntity.getContent(),
                 teamEntity.getSubjectType(),
                 teamEntity.getOpenChatUrl(),
-                Collections.emptyList()
+                team.hashTag()
         ));
     }
 
     @DisplayName("제목이 null이면 에러를 반환한다.")
     @Test
     void nullSubjectInvalidException() {
-        TeamCreateRequest request = TeamCreateRequestFixture.create(null);
+        final TeamCommand command = TeamCommandFixture.create(null);
 
         assertThatThrownBy(() -> {
-            teamUseCase.save(request);
+            teamUseCase.save(command);
         }).isInstanceOf(DataIntegrityViolationException.class);
     }
 
     @DisplayName("해시태그를 저장한다.")
     @Test
     void saveHashTag() {
-        TeamCreateRequest request = TeamCreateRequestFixture.create("제목", List.of("스프링"));
+        final TeamCommand command = TeamCommandFixture.create("제목", List.of("스프링"));
 
-        Team team = teamUseCase.save(request);
+        Team team = teamUseCase.save(command);
 
         TeamEntity teamEntity = teamRepository.findById(team.id())
                 .orElseThrow(RuntimeException::new);
@@ -98,9 +97,9 @@ public class TeamUseCaseTest extends ServiceTest {
     @DisplayName("해시태그 글자가 11개 이상이면 에러가 발생한다.")
     @Test
     void isMaxLengthHashTag() {
-        TeamCreateRequest request = TeamCreateRequestFixture.create("제목", List.of("Spring framework"));
+        final TeamCommand command = TeamCommandFixture.create("제목", List.of("Spring framework"));
         assertThatThrownBy(() -> {
-            teamUseCase.save(request);
+            teamUseCase.save(command);
         });
     }
 
