8eee1d6f65dbf42374693486afa783a05cb69566
신성수 <tlstjdtn321@naver.com>
2023-12-27 16:36:57 +0900

    refactor: Serivce 단 Request -> Command로 수정

--- team-service/src/main/java/com/guin/team/application/service/TeamService.java
+++ team-service/src/main/java/com/guin/team/application/service/TeamService.java
@@ -1,10 +1,9 @@
 package com.guin.team.application.service;
 
-import com.guin.team.adapter.in.web.dto.request.TeamCreateRequest;
-import com.guin.team.application.service.dto.TeamCommand;
-import com.guin.team.domain.vo.Team;
 import com.guin.team.application.port.in.TeamUseCase;
+import com.guin.team.application.port.in.command.TeamCommand;
 import com.guin.team.application.port.out.TeamPort;
+import com.guin.team.domain.vo.Team;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,15 +16,7 @@ public class TeamService implements TeamUseCase {
     private final TeamPort teamPort;
 
     @Override
-    public Team save(final TeamCreateRequest request) {
-        return teamPort.save(
-                new TeamCommand(
-                        request.subject(),
-                        request.content(),
-                        request.subjectType(),
-                        request.openChatUrl(),
-                        request.hashTags()
-                )
-        );
+    public Team save(final TeamCommand command) {
+        return teamPort.save(command);
     }
 }
