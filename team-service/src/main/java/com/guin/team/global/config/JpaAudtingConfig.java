af0f5a5fb1aae6347d995825333ff35d3fbba99b
신성수 <tlstjdtn321@naver.com>
2023-12-28 17:39:43 +0900

    refactor: 설정 위치 수정

--- team-service/src/main/java/com/guin/team/global/config/JpaAudtingConfig.java
+++ team-service/src/main/java/com/guin/team/global/config/JpaAudtingConfig.java
@@ -1,4 +1,4 @@
-package com.guin.team.config;
+package com.guin.team.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
