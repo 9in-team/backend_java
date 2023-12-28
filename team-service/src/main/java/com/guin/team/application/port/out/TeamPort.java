00287cd23eda90267598d5c615a2a9f62abacb31
신성수 <tlstjdtn321@naver.com>
2023-12-28 17:40:55 +0900

    feat: command 에서 domain으로 수정하도록 수정

--- team-service/src/main/java/com/guin/team/application/port/out/TeamPort.java
+++ team-service/src/main/java/com/guin/team/application/port/out/TeamPort.java
@@ -5,6 +5,6 @@ import com.guin.team.domain.vo.Team;
 
 public interface TeamPort {
 
-    Team save(TeamCommand teamCommand);
+    Team save(Team team);
 
 }
--- team-service/src/main/java/com/guin/team/application/service/TeamService.java
+++ team-service/src/main/java/com/guin/team/application/service/TeamService.java
@@ -4,19 +4,52 @@ import com.guin.team.application.port.in.TeamUseCase;
 import com.guin.team.application.port.in.command.TeamCommand;
 import com.guin.team.application.port.out.TeamPort;
 import com.guin.team.domain.vo.Team;
+import com.guin.team.domain.vo.TeamRole;
+import com.guin.team.domain.vo.TeamTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class TeamService implements TeamUseCase {
 
+    private static final Long DEFAULT_ID = 1L;
+    private static final String DELIMITER = ",";
+
     private final TeamPort teamPort;
 
     @Override
     public Team save(final TeamCommand command) {
-        return teamPort.save(command);
+        final List<TeamTemplate> teamTemplates = command.teamTemplates().stream()
+                .map(teamTemplate -> {
+                    List<TeamTemplate.CheckboxTemplate> checkboxTemplates = Arrays.stream(teamTemplate.option().split(DELIMITER))
+                            .map(option -> new TeamTemplate.CheckboxTemplate(DEFAULT_ID, option))
+                            .toList();
+
+                    return new TeamTemplate(DEFAULT_ID, teamTemplate.type(), teamTemplate.question(), checkboxTemplates);
+                })
+                .toList();
+
+        final List<TeamRole> teamRoles = command.teamRoles().stream()
+                .map(role -> new TeamRole(DEFAULT_ID, role.name(), role.requiredCount(), 0))
+                .toList();
+
+        final Team team = new Team(
+                DEFAULT_ID,
+                DEFAULT_ID,
+                command.subject(),
+                command.content(),
+                command.subjectType(),
+                command.openChatUrl(),
+                command.hashTags(),
+                teamRoles,
+                teamTemplates);
+
+        return teamPort.save(team);
     }
 }
