85047d4d439ea56b3cb0ba46d61bcd6083a54c21
신성수 <tlstjdtn321@naver.com>
2023-12-28 17:41:30 +0900

    fix: command로 넘기는 로직 수정

--- team-service/src/main/java/com/guin/team/adapter/out/persistence/TeamPersistenceAdapter.java
+++ team-service/src/main/java/com/guin/team/adapter/out/persistence/TeamPersistenceAdapter.java
@@ -1,14 +1,19 @@
 package com.guin.team.adapter.out.persistence;
 
 import com.guin.team.adapter.out.persistence.entity.TeamEntity;
+import com.guin.team.adapter.out.persistence.entity.TeamRoleEntity;
+import com.guin.team.adapter.out.persistence.entity.TeamTemplateEntity;
 import com.guin.team.adapter.out.persistence.mapper.HashTagMapper;
 import com.guin.team.adapter.out.persistence.mapper.TeamMapper;
+import com.guin.team.adapter.out.persistence.mapper.TeamRoleMapper;
+import com.guin.team.adapter.out.persistence.mapper.TeamTemplateMapper;
 import com.guin.team.adapter.out.persistence.repository.TeamRepository;
-import com.guin.team.application.port.in.command.TeamCommand;
-import com.guin.team.domain.vo.Team;
 import com.guin.team.application.port.out.TeamPort;
+import com.guin.team.domain.vo.Team;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -17,19 +22,29 @@ public class TeamPersistenceAdapter implements TeamPort {
     private final TeamRepository teamRepository;
     private final TeamMapper teamMapper;
     private final HashTagMapper hashTagMapper;
+    private final TeamRoleMapper teamRoleMapper;
+    private final TeamTemplateMapper teamTemplateMapper;
 
     @Override
-    public Team save(final TeamCommand teamCommand) {
-        // TODO: 회원 서버와 연동이되면 그때 저장 가능.
-        return teamMapper.toMapper(
-                teamRepository.save(new TeamEntity(
-                        1L,
-                        teamCommand.subject(),
-                        teamCommand.content(),
-                        teamCommand.subjectType(),
-                        teamCommand.openChatUrl(),
-                        hashTagMapper.toEntity(teamCommand.hashTags())
-                ))
-        );
+    public Team save(final Team team) {
+        final List<TeamRoleEntity> teamRoles = team.teamRoles().stream()
+                .map(teamRoleMapper::toEntity)
+                .toList();
+        final List<TeamTemplateEntity> teamTemplateEntities = team.teamTemplates().stream()
+                .map(teamTemplateMapper::toEntity)
+                .toList();
+        final TeamEntity teamEntity = new TeamEntity(
+                team.leaderId(),
+                team.subject(),
+                team.content(),
+                team.subjectType(),
+                team.openChatUrl(),
+                hashTagMapper.toEntity(team.hashTag()),
+                teamRoles,
+                teamTemplateEntities);
+
+        final TeamEntity newTeamEntity = teamRepository.save(teamEntity);
+
+        return teamMapper.toDomain(newTeamEntity);
     }
 }
