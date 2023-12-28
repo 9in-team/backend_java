f3d571e905873038d80f59c4745ed0fed9252b9d
신성수 <tlstjdtn321@naver.com>
2023-12-28 17:40:25 +0900

    feat: 팀 모집 Command 추가

--- team-service/src/main/java/com/guin/team/application/port/in/command/TeamCommand.java
+++ team-service/src/main/java/com/guin/team/application/port/in/command/TeamCommand.java
@@ -1,6 +1,7 @@
 package com.guin.team.application.port.in.command;
 
 import com.guin.team.domain.constant.SubjectType;
+import com.guin.team.domain.constant.TemplateType;
 
 import java.util.List;
 
@@ -9,5 +10,20 @@ public record TeamCommand(
         String content,
         SubjectType subjectType,
         String openChatUrl,
-        List<String> hashTags
-) { }
+        List<String> hashTags,
+        List<TeamTemplate> teamTemplates,
+        List<TeamRole> teamRoles
+) {
+
+    public record TeamTemplate(
+            TemplateType type,
+            String question,
+            String option
+    ) { }
+
+    public record TeamRole(
+            String name,
+            int requiredCount
+    ) { }
+
+}
