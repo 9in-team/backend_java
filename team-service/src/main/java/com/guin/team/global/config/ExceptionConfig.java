3a2854ad67c2d57e0ab83a8905614205b69f2bc6
신성수 <tlstjdtn321@naver.com>
2024-01-02 17:48:18 +0900

    chore: 모듈 exception을 사용하기 위해 설정 추가

---
+++ team-service/src/main/java/com/guin/team/global/config/ExceptionConfig.java
@@ -0,0 +1,9 @@
+package com.guin.team.global.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.guin.exception")
+public class ExceptionConfig {
+}
