f3fa879f9e731f00c705a38df7697a7177b02625
신성수 <tlstjdtn321@naver.com>
2024-01-02 17:49:37 +0900

    test: Team domain 테스트 매퍼 추가

---
+++ team-service/src/test/java/com/guin/team/mapper/TeamCreateResponseTestMapper.java
@@ -0,0 +1,71 @@
+package com.guin.team.mapper;
+
+import com.guin.team.adapter.in.web.dto.response.TeamCreateResponse;
+import com.guin.team.adapter.out.persistence.entity.HashTagEntity;
+import com.guin.team.adapter.out.persistence.entity.TeamEntity;
+import com.guin.team.adapter.out.persistence.entity.TeamRoleEntity;
+import com.guin.team.adapter.out.persistence.entity.TeamTemplateEntity;
+import com.guin.team.domain.constant.SubjectType;
+import io.restassured.path.json.JsonPath;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TeamCreateResponseTestMapper {
+
+    public static TeamCreateResponse create(TeamEntity teamEntity) {
+        return new TeamCreateResponse(
+                teamEntity.getId(),
+                teamEntity.getOpenChatUrl(),
+                teamEntity.getContent(),
+                teamEntity.getSubject(),
+                teamEntity.getSubjectType(),
+                toTemplateDetail(teamEntity.getTeamTemplates()),
+                toHashTag(teamEntity.getHashTags()),
+                toRoleDetail(teamEntity.getTeamRoles())
+        );
+    }
+
+    private static List<TeamCreateResponse.TeamRoleDetail> toRoleDetail(List<TeamRoleEntity> teamRoleEntities) {
+        return teamRoleEntities.stream()
+                .map(teamRoleEntity -> new TeamCreateResponse.TeamRoleDetail(
+                        teamRoleEntity.getName(),
+                        teamRoleEntity.getRequiredCount(),
+                        teamRoleEntity.getHiredCount()
+                ))
+                .toList();
+    }
+
+    private static List<String> toHashTag(List<HashTagEntity> hashTagEntities) {
+        return hashTagEntities.stream()
+                .map(HashTagEntity::getHashTag)
+                .toList();
+    }
+
+    private static List<TeamCreateResponse.TeamTemplateDetail> toTemplateDetail(List<TeamTemplateEntity> teamTemplateEntities) {
+        return teamTemplateEntities.stream()
+                .map(teamTemplateEntity ->  new TeamCreateResponse.TeamTemplateDetail(
+                        teamTemplateEntity.getTemplateType(),
+                        teamTemplateEntity.getQuestion(),
+                        teamTemplateEntity.getCheckBoxTemplates().getCheckBoxTemplates().stream()
+                                .map(checkBoxTemplateEntity -> checkBoxTemplateEntity.getOptionName())
+                                .collect(Collectors.joining(","))
+                ))
+                .toList();
+    }
+
+    public static TeamCreateResponse create(JsonPath response) {
+        return new TeamCreateResponse(
+                response.getLong("teamId"),
+                response.getString("openChatUrl"),
+                response.getString("content"),
+                response.getString("subject"),
+                SubjectType.from(response.getString("subjectType")),
+                response.getList("teamTemplates"),
+                response.getList("hashTags"),
+                response.getList("teamRoleDetail")
+        );
+    }
+
+
+}
---
+++ team-service/src/test/java/com/guin/team/mapper/TeamTestMapper.java
@@ -0,0 +1,43 @@
+package com.guin.team.mapper;
+
+import com.guin.team.adapter.out.persistence.entity.CheckBoxTemplateEntity;
+import com.guin.team.adapter.out.persistence.entity.TeamRoleEntity;
+import com.guin.team.adapter.out.persistence.entity.TeamTemplateEntity;
+import com.guin.team.domain.vo.TeamRole;
+import com.guin.team.domain.vo.TeamTemplate;
+
+import java.util.List;
+
+public class TeamTestMapper {
+
+    public static List<TeamRole> toTeamRoles(final List<TeamRoleEntity> teamRoleEntities) {
+        return teamRoleEntities.stream()
+                .map(teamRole -> new TeamRole(
+                        teamRole.getId(),
+                        teamRole.getName(),
+                        teamRole.getRequiredCount(),
+                        teamRole.getHiredCount()
+                )).toList();
+    }
+
+    public static List<TeamTemplate> toTeamTempalte(List<TeamTemplateEntity> teamTemplateEntities) {
+        return teamTemplateEntities.stream()
+                .map(teamTemplateEntity -> new TeamTemplate(
+                        teamTemplateEntity.getId(),
+                        teamTemplateEntity.getTemplateType(),
+                        teamTemplateEntity.getQuestion(),
+                        toCheckBoxTemplate(teamTemplateEntity.getCheckBoxTemplates().getCheckBoxTemplates())
+                ))
+                .toList();
+    }
+
+    private static List<TeamTemplate.CheckboxTemplate> toCheckBoxTemplate(List<CheckBoxTemplateEntity> checkBoxTemplateEntities) {
+        return checkBoxTemplateEntities.stream()
+                .map(checkBoxTemplateEntity -> new TeamTemplate.CheckboxTemplate(
+                        checkBoxTemplateEntity.getId(),
+                        checkBoxTemplateEntity.getOptionName()
+                ))
+                .toList();
+    }
+
+}
