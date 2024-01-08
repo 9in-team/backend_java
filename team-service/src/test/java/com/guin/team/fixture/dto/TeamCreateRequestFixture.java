383b987ed410a4fd34400d7d510f5b77964e9e17
Shin seongsu <tlstjdtn321@naver.com>
2024-01-09 03:15:12 +0900

    test: 피처스 수정

--- team-service/src/test/java/com/guin/team/fixture/dto/TeamCreateRequestFixture.java
+++ team-service/src/test/java/com/guin/team/fixture/dto/TeamCreateRequestFixture.java
@@ -2,40 +2,61 @@ package com.guin.team.fixture.dto;
 
 import com.guin.team.adapter.in.web.dto.request.TeamCreateRequest;
 import com.guin.team.domain.constant.SubjectType;
+import com.guin.team.domain.constant.TemplateType;
 import com.guin.team.fixture.Fixture;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
 public class TeamCreateRequestFixture extends Fixture {
 
-    public static TeamCreateRequest create(final String subject) {
-        String randomUUID = UUID.randomUUID().toString();
+    private static final List<TeamCreateRequest.TeamCreateTemplateRequest> DEFAULT_TEAM_TEMPLATE =
+            Collections.singletonList(new TeamCreateRequest.TeamCreateTemplateRequest(TemplateType.CHECKBOX, "열심히 하실 건가요?", "네,아니요"));
+    private static final List<String> DEFAULT_HASHTAG = Collections.singletonList("스프링");
+    private static final List<TeamCreateRequest.TeamCreateRoleRequest> DEFAULT_TEAM_ROLE =
+            Collections.singletonList(new TeamCreateRequest.TeamCreateRoleRequest("백엔드", 2));
 
+    public static TeamCreateRequest create(final String subject,
+                                           final List<TeamCreateRequest.TeamCreateTemplateRequest> teamCreateTemplateRequests,
+                                           final List<String> hashTags,
+                                           final List<TeamCreateRequest.TeamCreateRoleRequest> teamCreateRoleRequests) {
         return new TeamCreateRequest(
                 subject,
-                "content " + randomUUID,
-                randomUUID,
+                "content",
+                "https://kakaotalk.com/gieofgqnflq24",
                 SubjectType.PROJECT,
-                Collections.emptyList(),
-                Collections.emptyList(),
-                Collections.emptyList()
+                teamCreateTemplateRequests,
+                hashTags,
+                teamCreateRoleRequests
         );
     }
 
-    public static TeamCreateRequest create(final String subject, final List<String> hashTags) {
-        String randomUUID = UUID.randomUUID().toString();
+    public static TeamCreateRequest create(final String subject) {
+        return create(subject,
+                DEFAULT_TEAM_TEMPLATE,
+                DEFAULT_HASHTAG,
+                DEFAULT_TEAM_ROLE);
+    }
 
-        return new TeamCreateRequest(
-                subject,
-                "content " + randomUUID,
-                randomUUID,
-                SubjectType.PROJECT,
-                Collections.emptyList(),
+    public static TeamCreateRequest create(final String subject, final List<String> hashTags) {
+        return create(subject,
+                DEFAULT_TEAM_TEMPLATE,
                 hashTags,
-                Collections.emptyList()
-        );
+                DEFAULT_TEAM_ROLE);
+    }
+
+    public static TeamCreateRequest create(final String subject, final TeamCreateRequest.TeamCreateTemplateRequest teamCreateTemplateRequests) {
+        return create(subject,
+                Collections.singletonList(teamCreateTemplateRequests),
+                DEFAULT_HASHTAG,
+                DEFAULT_TEAM_ROLE);
+    }
+
+    public static TeamCreateRequest create(final String subject, final TeamCreateRequest.TeamCreateRoleRequest teamCreateTemplateRequests) {
+        return create(subject,
+                DEFAULT_TEAM_TEMPLATE,
+                DEFAULT_HASHTAG,
+                Collections.singletonList(teamCreateTemplateRequests));
     }
 
 }
