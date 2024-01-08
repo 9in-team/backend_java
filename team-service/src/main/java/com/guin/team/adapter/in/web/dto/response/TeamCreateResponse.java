a7576daf496ddd1261282819621eb56acf07a115
Shin seongsu <tlstjdtn321@naver.com>
2024-01-09 03:09:22 +0900

    fuix: option 이름 Reqeust와 동일시

--- team-service/src/main/java/com/guin/team/adapter/in/web/dto/response/TeamCreateResponse.java
+++ team-service/src/main/java/com/guin/team/adapter/in/web/dto/response/TeamCreateResponse.java
@@ -19,7 +19,7 @@ public record TeamCreateResponse(
     public record TeamTemplateDetail(
         TemplateType type,
         String question,
-        String options
+        String option
     ) { }
 
     public record TeamRoleDetail(
