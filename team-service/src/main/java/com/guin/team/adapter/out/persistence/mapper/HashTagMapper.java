cca70fac8ec7969ca0a4001c11bd13a46ae8f417
Shin seongsu <tlstjdtn321@naver.com>
2024-01-09 03:12:19 +0900

    refactor: mapper 로직 수정

--- team-service/src/main/java/com/guin/team/adapter/out/persistence/mapper/HashTagMapper.java
+++ team-service/src/main/java/com/guin/team/adapter/out/persistence/mapper/HashTagMapper.java
@@ -3,15 +3,20 @@ package com.guin.team.adapter.out.persistence.mapper;
 import com.guin.team.adapter.out.persistence.entity.HashTagEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
 public class HashTagMapper {
 
     public List<HashTagEntity> toEntity(final List<String> hashTags) {
-        return hashTags.stream()
-                .map(HashTagEntity::new)
-                .collect(Collectors.toList());
+        return Optional.ofNullable(hashTags)
+                .map(hashTag -> hashTag.stream()
+                        .map(HashTagEntity::new)
+                        .collect(Collectors.toList())
+                )
+                .orElse(Collections.emptyList());
     }
 }
