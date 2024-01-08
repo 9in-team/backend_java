f77144bd3345a7a1a6064ffd92e47de38526d434
Shin seongsu <tlstjdtn321@naver.com>
2024-01-09 03:13:31 +0900

    test: 문서화 템플릿 및 팀원 반환되도록 수정

--- team-service/src/test/java/com/guin/team/documentation/step/TeamDocumentStep.java
+++ team-service/src/test/java/com/guin/team/documentation/step/TeamDocumentStep.java
@@ -9,6 +9,7 @@ import static com.guin.team.documentation.DocumentPreprocessor.requestPreprocess
 import static com.guin.team.documentation.DocumentPreprocessor.responsePreprocessor;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 
 public class TeamDocumentStep {
 
@@ -28,9 +29,14 @@ public class TeamDocumentStep {
                 fieldWithPath("content").type(JsonFieldType.STRING).description("모집글 내용"),
                 fieldWithPath("openChatUrl").type(JsonFieldType.STRING).description("오픈채팅방 URL"),
                 fieldWithPath("subjectType").type(JsonFieldType.STRING).description("프로젝트 종류"),
-                fieldWithPath("teamTemplates").type(JsonFieldType.ARRAY).description("질문 템플렛").optional(),
+                fieldWithPath("teamTemplates").type(JsonFieldType.ARRAY).description("질문 템플렛"),
+                fieldWithPath("teamTemplates[].type").type(JsonFieldType.STRING).description("질문 양식"),
+                fieldWithPath("teamTemplates[].question").type(JsonFieldType.STRING).description("질문 내용"),
+                fieldWithPath("teamTemplates[].option").type(JsonFieldType.STRING).description("양식이 체크박스 일시 항목"),
                 fieldWithPath("hashTags").type(JsonFieldType.ARRAY).description("해시태그"),
-                fieldWithPath("roles").type(JsonFieldType.ARRAY).description("포지션별 참여 인원").optional()
+                fieldWithPath("roles").type(JsonFieldType.ARRAY).description("포지션별 참여 인원"),
+                fieldWithPath("roles[].name").type(JsonFieldType.STRING).description("포지션 이름"),
+                fieldWithPath("roles[].requiredCount").type(JsonFieldType.NUMBER).description("필요 인원 수")
         );
     }
 
@@ -41,9 +47,15 @@ public class TeamDocumentStep {
                 fieldWithPath("content").type(JsonFieldType.STRING).description("모집글 내용"),
                 fieldWithPath("subject").type(JsonFieldType.STRING).description("모집글 제목"),
                 fieldWithPath("subjectType").type(JsonFieldType.STRING).description("프로젝트 종류"),
-                fieldWithPath("teamTemplates").type(JsonFieldType.ARRAY).description("질문 템플렛").optional(),
+                fieldWithPath("teamTemplates").type(JsonFieldType.ARRAY).description("질문 템플렛"),
+                fieldWithPath("teamTemplates[].type").type(JsonFieldType.STRING).description("질문 양식"),
+                fieldWithPath("teamTemplates[].question").type(JsonFieldType.STRING).description("질문 내용"),
+                fieldWithPath("teamTemplates[].option").type(JsonFieldType.STRING).description("양식이 체크박스 일시 항목"),
                 fieldWithPath("hashTags").type(JsonFieldType.ARRAY).description("해시태그"),
-                fieldWithPath("teamRoleDetail").type(JsonFieldType.ARRAY).description("포지션별 참여 인원").optional()
+                fieldWithPath("teamRoleDetail").type(JsonFieldType.ARRAY).description("포지션별 참여 인원"),
+                fieldWithPath("teamRoleDetail[].name").type(JsonFieldType.STRING).description("포지션 이름"),
+                fieldWithPath("teamRoleDetail[].requiredCount").type(JsonFieldType.NUMBER).description("필요 인원 수"),
+                fieldWithPath("teamRoleDetail[].hiredCount").type(JsonFieldType.NUMBER).description("참여한 인원 수")
         );
     }
 
