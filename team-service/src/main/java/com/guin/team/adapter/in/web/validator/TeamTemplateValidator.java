dd58559d44459ad2f364a3a09c82cf26df383f8c
Shin seongsu <tlstjdtn321@naver.com>
2024-01-09 03:10:47 +0900

    feat: 템플릿 체크로직 추가

--- team-service/src/main/java/com/guin/team/adapter/in/web/validator/TeamTemplateValidator.java
+++ team-service/src/main/java/com/guin/team/adapter/in/web/validator/TeamTemplateValidator.java
@@ -2,8 +2,10 @@ package com.guin.team.adapter.in.web.validator;
 
 import com.guin.team.adapter.in.web.dto.request.TeamCreateRequest;
 import com.guin.team.adapter.in.web.validator.annotation.TeamTemplate;
+import com.guin.team.domain.constant.TemplateType;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.hibernate.validator.internal.engine.constraintvalidation.ConstraintValidatorContextImpl;
 
 import java.util.List;
 import java.util.Objects;
@@ -11,23 +13,37 @@ import java.util.Objects;
 public class TeamTemplateValidator implements ConstraintValidator<TeamTemplate, List<TeamCreateRequest.TeamCreateTemplateRequest>> {
 
     private static final int MAX_QUESTION_LENGTH = 250;
+    private static final String ENOGHT_CHECKBOX_OPTION = "템플릿이 체크박스일경우에는 옵션이 필수 입니다.";
 
     @Override
     public boolean isValid(List<TeamCreateRequest.TeamCreateTemplateRequest> teamCreateTemplateRequests, ConstraintValidatorContext constraintValidatorContext) {
-        if (Objects.isNull(teamCreateTemplateRequests)) {
+        if (teamCreateTemplateRequests == null) {
             return true;
         }
 
-        if (maxLengthQuestionValidate(teamCreateTemplateRequests)) {
-            return true;
+        if (isMaxLengthQuestionValidate(teamCreateTemplateRequests)) {
+            return false;
         }
 
-        return false;
+        if (isCheckboxTemplateOptionValidate(teamCreateTemplateRequests)) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate(ENOGHT_CHECKBOX_OPTION)
+                    .addConstraintViolation();
+            return false;
+        }
+
+        return true;
     }
 
-    private boolean maxLengthQuestionValidate(final List<TeamCreateRequest.TeamCreateTemplateRequest> teamCreateTemplateRequests) {
+    private boolean isMaxLengthQuestionValidate(final List<TeamCreateRequest.TeamCreateTemplateRequest> teamCreateTemplateRequests) {
         return teamCreateTemplateRequests.stream()
-                .noneMatch(teamCreateTemplateRequest -> teamCreateTemplateRequest.question().length() > MAX_QUESTION_LENGTH);
+                .anyMatch(teamCreateTemplateRequest -> teamCreateTemplateRequest.question().length() > MAX_QUESTION_LENGTH);
+    }
+
+    private boolean isCheckboxTemplateOptionValidate(final List<TeamCreateRequest.TeamCreateTemplateRequest> teamCreateTemplateRequests) {
+        return teamCreateTemplateRequests.stream()
+                .anyMatch(teamCreateTemplateRequest -> teamCreateTemplateRequest.type() == TemplateType.CHECKBOX
+                                                    && teamCreateTemplateRequest.option() == null);
     }
 
 }
