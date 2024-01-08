78cc548924858b61c777fe37b03db9d94bf764e2
Shin seongsu <tlstjdtn321@naver.com>
2024-01-09 03:11:56 +0900

    fix: 빈 생성자 생성되도록 수정

--- team-service/src/main/java/com/guin/team/adapter/out/persistence/entity/CheckBoxTemplates.java
+++ team-service/src/main/java/com/guin/team/adapter/out/persistence/entity/CheckBoxTemplates.java
@@ -6,13 +6,16 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Objects;
 
 @Embeddable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CheckBoxTemplates {
 
     @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
@@ -32,7 +35,7 @@ public class CheckBoxTemplates {
     private static void validate(final List<CheckBoxTemplateEntity> checkBoxTemplates,
                                  final TemplateType templateType) {
         if(templateType == TemplateType.CHECKBOX) {
-            if(Objects.isNull(checkBoxTemplates)) {
+            if(checkBoxTemplates == null) {
                 throw new InvalidCheckBoxTemplateException();
             }
         }
