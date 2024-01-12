97d75403d85a440c0b3a211b06b1ac5e796c044b
신성수 <tlstjdtn321@naver.com>
2024-01-12 13:38:45 +0900

    feat: 에러메시지 추가

--- storage/sharding/src/main/java/com/guin/storage/sharding/error/InvalidShardKeyException.java
+++ storage/sharding/src/main/java/com/guin/storage/sharding/error/InvalidShardKeyException.java
@@ -1,4 +1,10 @@
 package com.guin.storage.sharding.error;
 
+import com.guin.storage.sharding.error.message.ErrorMessage;
+
 public class InvalidShardKeyException extends RuntimeException{
+
+    public InvalidShardKeyException() {
+        super(ErrorMessage.INVALID_SHARDING_KEY.getMessage());
+    }
 }
